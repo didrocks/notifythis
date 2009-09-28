8fddfdd0501933ffd6c3302cb02b162f319f306b
Didier Roche <didrocks@gmail.com>
2009-09-28 21:57:52 +0200

    licensing

--- notifythis/daemon.py
+++ notifythis/daemon.py
@@ -2,7 +2,18 @@
 # -*- coding: utf-8 -*-
 # Modified from http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
 ### BEGIN LICENSE
-# This file is in the public domain
+# Copyright (C) 2009 Didier Roche <didrocks@ubuntu.com>
+#This program is free software: you can redistribute it and/or modify it 
+#under the terms of the GNU General Public License version 3, as published 
+#by the Free Software Foundation.
+#
+#This program is distributed in the hope that it will be useful, but 
+#WITHOUT ANY WARRANTY; without even the implied warranties of 
+#MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+#PURPOSE.  See the GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License along 
+#with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
 
