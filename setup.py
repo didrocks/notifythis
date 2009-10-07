0e2326ec6ca756589f92434952459780da619a18
Didier Roche <didrocks@gmail.com>
2009-10-07 18:27:54 +0200

    prepare setup.py

--- setup.py
+++ setup.py
@@ -25,81 +25,16 @@ except ImportError:
     sys.exit(1)
 
 assert DistUtilsExtra.auto.__version__ >= '2.10', 'needs DistUtilsExtra.auto >= 2.10'
-import os
 
-
-def update_data_path(prefix, oldvalue=None):
-
-    try:
-        fin = file('notifythis/notifythisconfig.py', 'r')
-        fout = file(fin.name + '.new', 'w')
-
-        for line in fin:            
-            fields = line.split(' = ') # Separate variable from value
-            if fields[0] == '__notifythis_data_directory__':
-                # update to prefix, store oldvalue
-                if not oldvalue:
-                    oldvalue = fields[1]
-                    line = "%s = '%s'\n" % (fields[0], prefix)
-                else: # restore oldvalue
-                    line = "%s = %s" % (fields[0], oldvalue)
-            fout.write(line)
-
-        fout.flush()
-        fout.close()
-        fin.close()
-        os.rename(fout.name, fin.name)
-    except (OSError, IOError), e:
-        print ("ERROR: Can't find notifythis/notifythisconfig.py")
-        sys.exit(1)
-    return oldvalue
-
-
-def update_desktop_file(datadir):
-
-    try:
-        fin = file('notifythis.desktop.in', 'r')
-        fout = file(fin.name + '.new', 'w')
-
-        for line in fin:            
-            if 'Icon=' in line:
-                line = "Icon=%s\n" % (datadir + 'media/icon.png')
-            fout.write(line)
-        fout.flush()
-        fout.close()
-        fin.close()
-        os.rename(fout.name, fin.name)
-    except (OSError, IOError), e:
-        print ("ERROR: Can't find notifythis.desktop.in")
-        sys.exit(1)
-
-
-class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
-    def run(self):
-        if self.root or self.home:
-            print "WARNING: You don't use a standard --prefix installation, take care that you eventually " \
-            "need to update quickly/quicklyconfig.py file to adjust __quickly_data_directory__. You can " \
-            "ignore this warning if you are packaging and uses --prefix."
-        previous_value = update_data_path(self.prefix + '/share/notifythis/')
-        update_desktop_file(self.prefix + '/share/notifythis/')
-        DistUtilsExtra.auto.install_auto.run(self)
-        update_data_path(self.prefix, previous_value)
-
-
-        
-##################################################################################
-###################### YOU SHOULD MODIFY ONLY WHAT IS BELOW ######################
-##################################################################################
 
 DistUtilsExtra.auto.setup(
     name='notifythis',
     version='0.1',
     license='GPL-3',
-    #author='Your Name',
-    #author_email='email@ubuntu.com',
-    #description='UI for managing …',
-    #long_description='Here a longer description',
-    #url='https://launchpad.net/notifythis',
-    cmdclass={'install': InstallAndUpdateDataDirectory}
+    author='Didier Roche',
+    author_email='didrocks@ubuntu.com',
+    description='Easily notify information to user from data file …',
+    long_description='NotifyThis is a daemon enabling notification from network or local xml files, showing icon, severity, caching data… The current use case is announcing conferences, lessons, as information during a party.',
+    url='https://launchpad.net/notifythis',
     )
 
